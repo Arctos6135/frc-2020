868903c3c29b3f823c63e7721a930e87565a3579
Tyler Tian <tylertian123@gmail.com>
2020-01-11 22:31:07 -0500

    Format and document TeleopDrive
    Use VS Code to auto format TeleopDrive.java. Add Javadocs to all methods
    except the ones overriding Command methods.

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -13,50 +13,102 @@ import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 
 public class TeleopDrive extends CommandBase {
-    private final Drivetrain driveTrain;
-    
-    //Controller related
-    static double DEADZONE = 0.15;
-    
-    //Steering or movement related
-    static boolean reverseDrive = false;
-    static boolean precisionDrive = false;  
-    static double precisionFactor = 0.5; //Percentage of default steering input when driving with precisionDrive
-    
-    //Ramping related
-    static double rampingRate = 1; //Time in seconds to go from 0 to full throttle.
+	private final Drivetrain driveTrain;
+
+	// Controller related
+	static double DEADZONE = 0.15;
+
+	// Steering or movement related
+	static boolean reverseDrive = false;
+	static boolean precisionDrive = false;
+	static double precisionFactor = 0.5; // Percentage of default steering input when driving with precisionDrive
+
+	// Ramping related
+	static double rampingRate = 1; // Time in seconds to go from 0 to full throttle.
 
 	public TeleopDrive(Drivetrain driveTrain) {
 		this.driveTrain = driveTrain;
 		addRequirements(driveTrain);
 	}
 
+	/**
+	 * Get whether the drive is reversed.
+	 * 
+	 * @return Whether the drive is reversed
+	 */
 	public static boolean isReversed() {
 		return reverseDrive;
 	}
+
+	/**
+	 * Set whether the drive should be reversed.
+	 * 
+	 * @param reverseDrive Whether the drive is reversed
+	 */
 	public static void setReverseDrive(boolean reverseDrive) {
 		TeleopDrive.reverseDrive = reverseDrive;
 	}
-	public static void toggleReverseDrive() {
-        reverseDrive = !reverseDrive;
-    }
 
+	/**
+	 * Toggle whether the drive is reversed.
+	 */
+	public static void toggleReverseDrive() {
+		reverseDrive = !reverseDrive;
+	}
 
 	public static boolean isPrecisionDrive() {
-        return precisionDrive;
+		return precisionDrive;
 	}
+
+	/**
+	 * Turn precision drive on or off.
+	 * 
+	 * <p>
+	 * In precision drive, the motor outputs are scaled down by a configurable
+	 * factor.
+	 * </p>
+	 * 
+	 * @param precisionDrive Whether the drive is in precision mode
+	 */
 	public static void setPrecisionDrive(boolean precisionDrive) {
-        TeleopDrive.precisionDrive = precisionDrive;
-	}	
+		TeleopDrive.precisionDrive = precisionDrive;
+	}
+
+	/**
+	 * Toggle precision drive.
+	 * 
+	 * <p>
+	 * In precision drive, the motor outputs are scaled down by a configurable
+	 * factor.
+	 * </p>
+	 */
 	public static void togglePrecisionDrive() {
-        precisionDrive = !precisionDrive;
-    }
+		precisionDrive = !precisionDrive;
+	}
+
+	/**
+	 * Set the drive output multiplier in precision mode.
+	 * 
+	 * @param factor The precision mode multiplier
+	 */
+	public static void setPrecisionFactor(double factor) {
+		precisionFactor = factor;
+	}
+
+	/**
+	 * Get the drive output multiplier in precision mode.
+	 * 
+	 * @return The precision mode multiplier
+	 */
+	public static double getPrecisionFactor() {
+		return precisionFactor;
+	}
 
 	@Override
-    public void initialize() {
-        driveTrain.setRamping(rampingRate);
+	public void initialize() {
+		driveTrain.setRamping(rampingRate);
 	}
-	
+
 	@Override
 	public void execute() {
 		double x = RobotContainer.driverController.getRawAxis(Constants.DRIVE_LEFT_RIGHT);
@@ -89,7 +141,7 @@ public class TeleopDrive extends CommandBase {
 
 	@Override
 	public void end(boolean interrupted) {
-        driveTrain.setRamping(0);
+		driveTrain.setRamping(0);
 		driveTrain.setMotors(0, 0);
 	}
 
