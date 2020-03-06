cee5eb1b1c351e4b6c7029dc5ce14f933b28c9ae
Tyler Tian <tylertian123@gmail.com>
2020-03-05 21:39:15 -0500

    Change Bryce 4 back to Talons and CIMs
    This reverts commit b1482e6c93c861659ed1e67d7a77fc5e7efff945.

--- src/main/java/frc/robot/subsystems/BryceFour.java
+++ src/main/java/frc/robot/subsystems/BryceFour.java
@@ -7,37 +7,44 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * The climber elevator.
+ */
 public class BryceFour extends SubsystemBase {
 
-    private final CANSparkMax leftMotor;
-    private final CANSparkMax rightMotor;
+    private final TalonSRX leftMotor;
+    private final TalonSRX rightMotor;
 
     public void setMotorSpeed(double motorSpeed) {
-        rightMotor.set(motorSpeed);
+        rightMotor.set(ControlMode.PercentOutput, motorSpeed);
     }
 
-	/**
-	* Creates a new BryceFour.
-	*/
-  	public BryceFour(int leftMotor, int rightMotor) {
-        this.leftMotor = new CANSparkMax(leftMotor, MotorType.kBrushless);
-        this.rightMotor = new CANSparkMax(rightMotor, MotorType.kBrushless);
+    /**
+     * Creates a new BryceFour.
+     */
+    public BryceFour(int leftMotor, int rightMotor) {
+        this.leftMotor = new TalonSRX(leftMotor);
+        this.rightMotor = new TalonSRX(rightMotor);
 
+        // Put motors on brake to prevent backdrive as much as possible
+        this.leftMotor.setNeutralMode(NeutralMode.Brake);
+        this.rightMotor.setNeutralMode(NeutralMode.Coast);
+        
         this.leftMotor.follow(this.rightMotor);
-        this.leftMotor.setIdleMode(IdleMode.kBrake);
-        this.rightMotor.setIdleMode(IdleMode.kBrake);
-        // limit to 20A
-        this.rightMotor.setSmartCurrentLimit(20);
-  	}
+        // Enable the current limit
+        // Once current exceeds 40A for 1.0s, limit current to 20A.
+        this.rightMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 20, 40, 1));
+    }
 
-  	@Override
-  	public void periodic() {
-		// This method will be called once per scheduler run
-  	}
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
